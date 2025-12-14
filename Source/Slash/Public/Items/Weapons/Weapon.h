8c8d0d819e536e5a86e70a1e95d4c7d67ab3da72
Malow <matthewlower22@gmail.com>
2025-12-14 17:36:22 -0500

    added box trace functionality for weapon

--- Source/Slash/Public/Items/Weapons/Weapon.h
+++ Source/Slash/Public/Items/Weapons/Weapon.h
@@ -7,18 +7,37 @@
 #include "Weapon.generated.h"
 
 class USoundBase;
+class UBoxComponent;
 
 UCLASS()
 class SLASH_API AWeapon : public AItem
 {
 	GENERATED_BODY()
 public:
+	AWeapon();
 	void Equip(USceneComponent* InParent, FName InSocketName);
 	void AttatchMeshToSocket(USceneComponent* InParent, const FName& InSocketName);
+
 protected:
+	virtual void BeginPlay() override;
+
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+	UFUNCTION()
+	void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
 private:
 	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
 	TObjectPtr<USoundBase> EquipSound;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	TObjectPtr<UBoxComponent> WeaponBox;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USceneComponent> BoxTraceStart;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USceneComponent> BoxTraceEnd;
 };
